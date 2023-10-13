980d415e4ff1c6b8a31558ff703bb9bc96ac946d
Amanda <mandacosta94@gmail.com>
2023-10-13 20:16:43 -0300

    feat: registration

--- app.py
+++ app.py
@@ -1,27 +1,50 @@
 from flask import Flask, session, render_template, request, g, redirect
+from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
-
 
 app = Flask(__name__)
 
+app.secret_key = 'sua_chave_secreta_aqui'
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
     db = get_db()
     cursor = db.cursor()
-    # users = cursor.execute("select * from users").fetchall()
 
     if request.method == "POST":      
         for input, value in request.form.items():
             if not value:
                 text = f"Error: {input} must me provided"
                 return render_template("register.html", erro=True, text=text)
+        email = request.form.get("email")
+        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+        user_exists = cursor.fetchone()
+        if user_exists:
+            text = f"Error: Email already being used."
+            return render_template("register.html", erro=True, text=text)
+        else:
+            name = request.form.get("name")
+            email = request.form.get("email")
+            birth = request.form.get("birthdate")
+            gender = request.form.get("gender")
+            hash = generate_password_hash(request.form.get("password"))
+            cursor.execute("INSERT INTO users (name, email, birth, gender, password_hash) VALUES (?, ?, ?, ?, ?)", (name, email, birth, gender, hash))
+            db.commit()
+            user_id = cursor.lastrowid
+            session["user_id"] = user_id
+            return redirect("/home")
 
     else:
         return render_template("register.html")
-
-    return render_template("register.html")
-
+    
 
 @app.route('/')
 def login():
@@ -31,6 +54,14 @@ def login():
     # users = cursor.execute("select * from users").fetchall()
     # users = cursor.execute("select * from users").fetchall()
     return render_template("login.html")
+
+
+@app.route('/home')
+def home():
+    db = get_db()
+    cursor = db.cursor()
+    return render_template("home.html")
+
 
 
 def get_db():
